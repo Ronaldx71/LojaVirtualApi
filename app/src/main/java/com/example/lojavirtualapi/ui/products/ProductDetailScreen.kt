c6d576212236f307ca862843312a79c708cd5b8c
Will-Andra <andrade.willi@hotmai.com>
2025-12-15 00:35:19 -0300

    feat: Adicionado Icone Grande no topo de CartDetailScreen e pequeno na CartListScreen

--- app/src/main/java/com/example/lojavirtualapi/ui/products/ProductDetailScreen.kt
+++ app/src/main/java/com/example/lojavirtualapi/ui/products/ProductDetailScreen.kt
@@ -6,10 +6,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.lojavirtualapi.api.RetrofitInstance
 import com.example.lojavirtualapi.model.Product
+import com.example.lojavirtualapi.ui.posts.PostDetailScreen
+
 
 @Composable
 fun ProductDetailScreen(id: Int, nav: NavController) {
